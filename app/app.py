284bfadb2496417731b5bf87f4852a83a3ee0095
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-11-25 23:05:16 +0530

    PY

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import requests
 
 
+
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
         movie_id)
@@ -11,6 +13,8 @@ def fetch_poster(movie_id):
     poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
+
+
 
 
 def recommend(movie):
