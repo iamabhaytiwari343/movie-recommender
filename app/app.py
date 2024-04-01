3dac2c7c5a2c294089da87b3b7286d6d5ece4956
Abhay Tiwari <iamabhaytiwari343@gmail.com>
2024-04-01 18:32:40 +0530

    app

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import requests
 
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+    url = "".format(
         movie_id)
     data = requests.get(url)
     data = data.json()
