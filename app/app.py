e856a17e2daa89256bedc24d9fdafa83795b6b15
Abhay Tiwari <iamabhaytiwari343@gmail.com>
2024-05-17 22:48:30 +0530

    app

--- app/app.py
+++ app/app.py
@@ -3,10 +3,8 @@ import streamlit as st
 import requests
 
 
-
-
 def fetch_poster(movie_id):
-    url = "".format(
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
         movie_id)
     data = requests.get(url)
     data = data.json()
@@ -15,12 +13,9 @@ def fetch_poster(movie_id):
     return full_path
 
 
-
-
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
-    distances = sorted(
-        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
@@ -32,11 +27,9 @@ def recommend(movie):
     return recommended_movie_names, recommended_movie_posters
 
 
-
-
 st.header('Movie Recommender System')
-movies = pickle.load(open('movie_list.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pickle.load(open(r'movie_list.pkl', 'rb'))
+similarity = pickle.load(open(r'similarity.pkl', 'rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -44,13 +37,9 @@ selected_movie = st.selectbox(
     movie_list
 )
 
-
-
-
 if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(
-        selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
